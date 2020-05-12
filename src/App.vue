f49e963faf90faf49778cdcdff5130a91d26551c
rcleend <rcleende@gmail.com>
2020-05-12 22:43:58 +0200

    Added and cleaned up Loading-logo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
     <div id="app">
-        <h1>Hoi</h1>
+        <loading v-if="true"></loading>
+        <router-view></router-view>
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator';
+    import Loading from '@/components/loading.vue';
 
-    @Component
+    @Component({
+        components: {
+            Loading,
+        },
+    })
     export default class App extends Vue {
     }
 </script>
 
+<style lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="home">
+        <h1>hoi</h1>
     </div>
 </template>
 
