c129853e80e7d9c21bc48b639a3bcf138f2719e3
rcleend <rcleende@gmail.com>
2020-05-12 19:35:50 +0200

    Added Vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="app">
+        <h1>Hoi</h1>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class App extends Vue {
+    }
+</script>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="home">
+    </div>
+</template>
+
+<script>
+    import {Component, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class Home extends Vue {
+    }
+</script>
